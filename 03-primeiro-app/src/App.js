279b15210ec5576a20c1174986fed550a769c482
Alberto Lima <alberto.btlima@outlook.com>
2024-08-23 17:01:43 -0300

    add: class card

--- 03-primeiro-app/src/App.js
+++ 03-primeiro-app/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Button from "./components/button/button.js";
+import Card from "./components/card/card.js";
 
 function App() {
   return (
     <>
       <Button />
       <Button />
+
+      <Card />
+      <Card />
     </>
   );
 }
